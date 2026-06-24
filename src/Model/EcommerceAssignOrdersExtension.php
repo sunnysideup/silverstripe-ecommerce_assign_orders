3fbc2b19aa34ab5d8be6abc3fc86621f15d2da55
SDGM <sion98madden@gmail.com>
2026-06-24 15:17:37 +1200

    Merge upgrades/automatedbranch/5to6 into main

--- src/Model/EcommerceAssignOrdersExtension.php
+++ src/Model/EcommerceAssignOrdersExtension.php
@@ -2,12 +2,13 @@
 
 namespace Sunnysideup\EcommerceAssignOrders\Model;
 
+use SilverStripe\Core\Extension;
+use Sunnysideup\Ecommerce\Model\Order;
 use SilverStripe\Control\Director;
 use SilverStripe\Core\Config\Config;
 use SilverStripe\Forms\DropdownField;
 use SilverStripe\Forms\FieldList;
 use SilverStripe\Forms\TextField;
-use SilverStripe\ORM\DataExtension;
 use SilverStripe\Security\Member;
 use Sunnysideup\Ecommerce\Email\OrderInvoiceEmail;
 use Sunnysideup\Ecommerce\Model\Extensions\EcommerceRole;
@@ -15,11 +16,11 @@ use Sunnysideup\Ecommerce\Model\Extensions\EcommerceRole;
 /**
  * Class \Sunnysideup\EcommerceAssignOrders\Model\EcommerceAssignOrdersExtension
  *
- * @property \Sunnysideup\Ecommerce\Model\Order|\Sunnysideup\EcommerceAssignOrders\Model\EcommerceAssignOrdersExtension $owner
+ * @property Order|EcommerceAssignOrdersExtension $owner
  * @property int $AssignedAdminID
- * @method \SilverStripe\Security\Member AssignedAdmin()
+ * @method Member AssignedAdmin()
  */
-class EcommerceAssignOrdersExtension extends DataExtension
+class EcommerceAssignOrdersExtension extends Extension
 {
     private static $has_one = [
         'AssignedAdmin' => Member::class,
@@ -47,10 +48,8 @@ class EcommerceAssignOrdersExtension extends DataExtension
     {
         if ($this->getOwner()->AssignedAdminID) {
             $admin = $this->getOwner()->AssignedAdmin();
-            if ($admin) {
-                if ($admin->exists()) {
-                    return $admin->getTitle();
-                }
+            if ($admin && $admin->exists()) {
+                return $admin->getTitle();
             }
         }
 
@@ -76,21 +75,17 @@ class EcommerceAssignOrdersExtension extends DataExtension
 
     public function onAfterWrite()
     {
-        if (Config::inst()->get(EcommerceAssignOrdersExtension::class, 'notify_by_email')) {
-            if ($this->getOwner()->AssignedAdminID) {
-                if ($this->getOwner()->isChanged('AssignedAdminID')) {
-                    //$member = $this->getOwner()->AssignedAdmin();
-                    $member = Member::get_by_id($this->getOwner()->AssignedAdminID);
-                    if ($member && $member->exists() && $member->Email) {
-                        $this->getOwner()->sendEmail(
-                            $emailClassName = OrderInvoiceEmail::class,
-                            $subject = 'An order has been assigned to you on ' . Director::absoluteURL('/'),
-                            $message = '<p>An order has been assigned to you:</p> <h1><a href="' . $this->getOwner()->CMSEditLink() . '">Open Order</a></h1>',
-                            $resend = true,
-                            $adminOnlyOrToEmail = $member->Email
-                        );
-                    }
-                }
+        if (Config::inst()->get(EcommerceAssignOrdersExtension::class, 'notify_by_email') && $this->getOwner()->AssignedAdminID && $this->getOwner()->isChanged('AssignedAdminID')) {
+            //$member = $this->getOwner()->AssignedAdmin();
+            $member = Member::get_by_id($this->getOwner()->AssignedAdminID);
+            if ($member && $member->exists() && $member->Email) {
+                $this->getOwner()->sendEmail(
+                    $emailClassName = OrderInvoiceEmail::class,
+                    $subject = 'An order has been assigned to you on ' . Director::absoluteURL('/'),
+                    $message = '<p>An order has been assigned to you:</p> <h1><a href="' . $this->getOwner()->CMSEditLink() . '">Open Order</a></h1>',
+                    $resend = true,
+                    $adminOnlyOrToEmail = $member->Email
+                );
             }
         }
     }
@@ -99,7 +94,7 @@ class EcommerceAssignOrdersExtension extends DataExtension
     {
         $shopAdminAndCurrentCustomerArray = EcommerceRole::list_of_admins(true);
         $titleArray = Config::inst()->get(EcommerceAssignOrdersExtension::class, 'searchable_fields');
-        $title = isset($titleArray['AssignedAdminID']['title']) ? $titleArray['AssignedAdminID']['title'] : 'Admin';
+        $title = $titleArray['AssignedAdminID']['title'] ?? 'Admin';
 
         return DropdownField::create(
             'AssignedAdminID',
